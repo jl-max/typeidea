9d3264f3289d9303c414692a9c9327f1658f9e7f
jl-max <1012634484@qq.com>
2025-07-21 14:29:53 +0800

    add SearchView and AuthorView

--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -14,20 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.urls import path
 from django.contrib import admin
 
-from blog.views import IndexView, CategoryView, TagView, PostDetailView
+from blog.views import IndexView, CategoryView, TagView, PostDetailView, SearchView, AuthorView
 from config.views import links
 from .custom_site import custom_site
 
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
-    path('category/<int:category_id>/', CategoryView.as_view(), name='category-list'),
-    path('tag/<int:tag_id>/', TagView.as_view(), name='tag-list'),
-    path('post/<int:post_id>.html', PostDetailView.as_view(), name='post-detail'),
-    path('links/', links, name='links'),
-    path('super_admin/', admin.site.urls, name='super-admin'),
-    path('admin/', custom_site.urls, name='admin'),
+    path("", IndexView.as_view(), name="index"),
+    path("search/", SearchView.as_view(), name="search"),
+    path("author/<int:owner_id>", AuthorView.as_view(), name="author"),
+    path("category/<int:category_id>/", CategoryView.as_view(), name="category-list"),
+    path("tag/<int:tag_id>/", TagView.as_view(), name="tag-list"),
+    path("post/<int:post_id>.html", PostDetailView.as_view(), name="post-detail"),
+    path("links/", links, name="links"),
+    path("super_admin/", admin.site.urls, name="super-admin"),
+    path("admin/", custom_site.urls, name="admin"),
 ]
