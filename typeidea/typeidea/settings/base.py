85d2502c2891488da1d87c3701449d243be377cf
jl-max <1012634484@qq.com>
2025-07-21 09:54:36 +0800

    change structure for templates

--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -54,11 +54,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'typeidea.urls'
-
+THEME = 'default'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'themes' / THEME / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
